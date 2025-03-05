5238468bd6403d98f1c37300a8f09e548713f86c
KikiMicky <kbs08052007@gmail.com>
2025-03-05 14:22:25 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import pygame 
-
+from grid import Grid
 
 # Initialize the pygame
 pygame.init()
 
-dark_blue = (44, 44, 125)
+dark_blue = (44, 44, 120)
 
 screen = pygame.display.set_mode ((300, 600))
 pygame.display.set_caption ("Python Tetris")
 
-clock = pygame.time.clock()
+clock = pygame.time.Clock()
+
+game_grid = Grid ()
+
+game_grid.grid [0][0] = 1
+game_grid.grid [3][4] = 4
+game_grid.grid [17][8] = 7
+
+
+game_grid.print_grid()
 
 while True:
     for event in pygame.event.get ():
@@ -18,6 +27,7 @@ while True:
             quit()
             
     # drawing
-    screen.fill (drak_blue)
+    screen.fill (dark_blue)
+    game_grid.draw(screen)
     pygame.display.update()
     clock.tick(60)     
